ad25bbe890981e714193aefcb598de02fe56a27e
Anirudh Tiwari <anirudh.tiwari@colorado.edu>
2017-11-05 03:02:35 -0700

    Server.c initial commit and PA3 pdf

---
+++ Distributed_System_For_Reliable_And_Secure_File_Storage/server/server.c
@@ -0,0 +1,30 @@
+/******************************************
+# Programming Assignment 3
+# server.c
+# Code Adapted from Professor's example-code
+# Date: November 12, 2017
+#Author: Anirudh Tiwari
+******************************************/
+
+/***** INCLUDES *****/
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <string.h>
+#include <dirent.h>
+#define MAXBUFSIZE 1024
+
+int main(){
+
+  return 0;
+}
