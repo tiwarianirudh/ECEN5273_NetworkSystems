f75112e8b654ee316ab60234cf807968f9dbcfbc
Anirudh Tiwari <anirudh.tiwari@colorado.edu>
2017-11-08 04:38:26 -0700

    Dont know what is working or what is not :(

--- Distributed_System_For_Reliable_And_Secure_File_Storage/client/client.c
+++ Distributed_System_For_Reliable_And_Secure_File_Storage/client/client.c
@@ -35,14 +35,19 @@ typedef struct{
   char password[4][64];
 }struct_parse;
 
-int parse_file(struct_parse *parse){
+typedef struct{
+  char username[64];
+  char password[64];
+  char command[32];
+  char filename[32];
+}struct_authenticate;
+
+int parse_file(struct_parse *parse, char* conf){
   FILE *fp;
   char line[MAXBUFSIZE];
   char str[MAXBUFSIZE]={0};
   char *c;
-  char *split;
-  int entry=0;
-  fp = fopen("dfc.conf", "r");
+  fp = fopen(conf, "r");
   if(!fp){
     printf("Error reading Configuration file\n");
     return -1;
@@ -104,20 +109,21 @@ Code by: https://stackoverflow.com/questions/10324611/how-to-calculate-the-md5-h
 char* MD5sum(char *filename){
   unsigned char hash_hex[MD5_DIGEST_LENGTH];
   int i;
-  char md5string[256];
-  FILE *inFile = fopen (filename, "rb");
+  char *md5string = malloc(sizeof(char)*256);
+  FILE *fp = fopen(filename, "rb");
   MD5_CTX mdContext;
-  int bytes;
-  unsigned char data[1024];
+  int nbytes;
+  unsigned char buffer[1024];
 
-  if (inFile == NULL) {
-      printf ("%s can't be opened.\n", filename);
+  if(fp == NULL) {
+      printf ("Error opening file for MD5SUM Calculation\n");
       return 0;
   }
 
   MD5_Init (&mdContext);
-  while ((bytes = fread (data, 1, 1024, inFile)) != 0)
-      MD5_Update (&mdContext, data, bytes);
+  while ((nbytes = fread(buffer, 1, 1024, fp)) != 0){
+    MD5_Update (&mdContext, buffer, nbytes);
+  }
   MD5_Final (hash_hex,&mdContext);
   for(i = 0; i < MD5_DIGEST_LENGTH; i++){
     //printf("%02x", c[i]);
@@ -125,18 +131,42 @@ char* MD5sum(char *filename){
   }
   printf("\n************\n");
   printf ("Hash Value: %s\n\n", md5string);
-  fclose (inFile);
-  return 0;
+  fclose(fp);
+  return (char *)md5string;
 }
 
-int main(){
+int* intMD5sum(char * hash_value){
+  int *hash_int = malloc(sizeof(int));
+  *hash_int = strtol(hash_value+31, NULL, 16);
+  printf("Integer Hash: %d\n", *hash_int );
+  return (int *)hash_int;
+}
+
+
+
+int main(int argc, char * argv[]){
   int parse_status;
   struct_parse parse;
+  struct_authenticate *auth = malloc(sizeof(struct_authenticate));
   char command[32];
   char *cname;
   char *filename;
+  char* conf;
+  int sockfd[4];
+  struct sockaddr_in server_addr, client_addr;
+  int nbytes;
 
-  parse_status = parse_file(&(parse));
+  int x; //Modulo4 value of hash_int
+
+  if (argc != 2)
+	{
+		printf ("\nUsage: <conf file>\n");
+		exit(1);
+	}
+  conf = argv[1];
+  //scanf(" %[^\n]s",conf);
+
+  parse_status = parse_file(&(parse), conf);
   if(parse_status==-1){
     printf("Error Parsing the Configuration File\n");
     exit(1);
@@ -145,16 +175,60 @@ int main(){
   /* Display the availble commands for the user */
   printf("\n\tEnter Command as under: \n");
   printf("\t1. list \n" "\t2. get <filename>\n" "\t3. put <filename>\n");
-  gets(command);	// store the command entered by the user
+  scanf(" %[^\n]s",command);	// store the command entered by the user
 
   cname = strdup(command);
   strtok(cname, " ");
   filename = strtok(NULL, " ");
   printf("Filename: %s\n", filename);
 
-  char* hash_value = MD5sum(filename);
+  char *hash_value = MD5sum(filename);
+  printf ("Hash Value in Main: %s\n\n", hash_value);
+
+  int *hash_int = intMD5sum(hash_value);
+  printf ("Hash Integer Value in Main: %d\n\n", *hash_int);
+
+  x = (*hash_int)%4;
+  printf("\n************\n");
+  printf("MD5HASH%%4 value: %d\n", x );
+
+
+
   if(!strcmp(cname, "put")){
+    printf("\n************\n");
     printf("Put File: \"%s\" on the server.\n", filename);
+
+    strcpy(auth->username, *parse.username);
+    strcpy(auth->password, *parse.password);
+    strcpy(auth->command, command);
+    strcpy(auth->filename, filename);
+    printf("Username:%sPassword:%sCommand:%sFilename:%s\n", auth->username, auth->password, auth->command, auth->filename );
+
+    for(int i=0; i<4; i++){
+      if((sockfd[i] = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+    		printf("Error in creating a socket for the Server:%s at Port: %d\n", parse.dfs[i], *parse.port_num[i]);
+    	}
+    	memset(&server_addr, 0, sizeof(server_addr));
+    	server_addr.sin_family = AF_INET;
+    	server_addr.sin_port = htons(*parse.port_num[i]);
+    	server_addr.sin_addr.s_addr = inet_addr(parse.dfs_ip[i]);
+      printf("\n\nSockfd %d: %d\n", i, sockfd[i]);
+      printf("%d\n", *(parse).port_num[i]);
+      printf("%s\n", (parse).dfs[i]);
+      printf("%s\n", (parse).dfs_ip[i]);
+    	if(connect(sockfd[i],(struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
+    	{
+        perror("Error: ");
+        printf("Error in Connecting to socket for the Server:%s at Port: %d\n", parse.dfs[i], *parse.port_num[i]);
+        continue;
+    	}
+
+      if((nbytes = send(sockfd[i], auth, sizeof(*auth), 0)) < 0){
+        printf("%d\n", nbytes);
+        printf("Error in sending to socket for the Server:%s at Port: %d\n", parse.dfs[i], *parse.port_num[i]);
+      }
+printf("Nbytes: %d\n", nbytes);
+    }
 
     FILE *fp;
     fp = fopen(filename, "rb");
--- Distributed_System_For_Reliable_And_Secure_File_Storage/server/server.c
+++ Distributed_System_For_Reliable_And_Secure_File_Storage/server/server.c
@@ -24,7 +24,96 @@
 #include <dirent.h>
 #define MAXBUFSIZE 1024
 
-int main(){
+int entry=0;
 
+typedef struct{
+  char username[4][64];
+  char password[4][64];
+}struct_parse;
+
+typedef struct{
+  char username[64];
+  char password[64];
+  char command[32];
+  char filename[32];
+}struct_authenticate;
+
+int parse_file(struct_parse *parse, char* conf){
+  FILE *fp;
+  char line[MAXBUFSIZE];
+
+  fp = fopen(conf, "r");
+  if(!fp){
+    printf("Error reading Configuration file\n");
+    return -1;
+  }
+  else{
+    while(fgets(line, sizeof(line), fp) > 0){
+        sscanf(line, "%s %s", (*parse).username[entry], (*parse).password[entry]);
+        printf("******From Conf File******\n");
+        printf("Username is: %s\n", (*parse).username[entry]);
+        printf("The password is: %s\n", (*parse).password[entry]);
+        entry++;
+    }
+    return 0;
+  }
+}
+
+int main(int argc, char * argv[]){
+  int parse_status;
+  struct_parse parse;
+  struct_authenticate *auth = malloc(sizeof(struct_authenticate));
+  int sockfd;
+  char port_num[32]= {0};
+  struct sockaddr_in server_addr, client_addr;
+  int newsockfd;
+  int length_client = sizeof(client_addr);
+
+  if (argc != 2)
+	{
+		printf ("\nUsage: <conf file>\n");
+		exit(1);
+	}
+  strcpy(port_num, argv[1]);
+  char *conf = "dfs.conf";
+  parse_status = parse_file(&(parse), conf);
+  if(parse_status==-1){
+    printf("Error Parsing the Configuration File\n");
+    exit(1);
+  }
+
+
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+    printf("Error creating socket \n");
+  }
+
+  bzero(&server_addr, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(atoi(port_num));
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+/*****binding the socket *****/
+  if(bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
+	   printf("unable to bind socket\n");
+     exit(-1);
+  }
+
+  printf("\n\n**********Waiting for New Connection**********\n\n");
+  if(listen(sockfd, 1024) < 0){
+    printf("*******Error in Listen*******\n");
+  }
+
+  while(1){
+    /***** Accepting new functions*****/
+    if((newsockfd = accept(sockfd, (struct sockaddr *)&client_addr, &length_client)) < 0){
+      printf("Error in accept. \n");
+      exit(-1);
+    }
+    printf("**********New Connection at Port %s - Socket : %d**********\n\n", port_num, newsockfd);
+  }
+  recv(newsockfd, auth, sizeof(*auth), 0);
+  for(int i=0; i<entry;i++){
+    printf("Username in struct_auth: %s\t\t Password: %s", &auth->username[i], &auth->password[i] );
+  }
   return 0;
 }
